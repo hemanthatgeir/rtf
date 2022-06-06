78b0c66536cd0738a72b4255b9c1559e0204aa1b
anantatgeir <106811344+anantatgeir@users.noreply.github.com>
2022-06-06 11:33:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ input_array = [{
     ],
     "anomalies": [
         "anomaly1",
-        "anomaliy2"
-    ]
+        "anomaly2"
+    ],
 },
     {
         "connection": "connection2",
@@ -25,35 +25,65 @@ input_array = [{
         ],
         "anomalies": [
             "anomaly1",
-            "anomaliy2"
+            "anomaly2"
         ]
     }
 ]
 
+temp_json = {"blocks": []}
+
 divider_json = {"type": "divider"}
-temp_dict = {"blocks": []}
-list_1 = []
 
-for element in range(len(input_array)):
-    for k, v in input_array[element].items():
-        if (isinstance(v, list)):
-            flat_list = ",".join(v)
-            list_1.append("%s:%s" % (k,v))
+tag_list = ["connection", "Batch", "job", "tests", "anomalies"]
+
+temp_dict = {}
+
+
+def extract_values(dict):
+    for tag in tag_list:
+        if (tag in dict.keys()):
+
+            if (isinstance(dict[tag], list)):
+                flat_list = "\n\t".join(dict[tag])
+                temp_dict[tag] = ":\n\t".join([tag, flat_list])
+            else:
+                temp_dict[tag] = ":".join([tag, dict[tag]])  ##temp_dict = {"connection":'connection:connection1'}
         else:
-            list_1.append("%s:%s" % (k,v))
+            temp_dict[tag] = ""
+    connection = temp_dict[tag_list[0]]
+    batch = temp_dict[tag_list[1]]
+    job = temp_dict[tag_list[2]]
+    tests = temp_dict[tag_list[3]]
+    anomalies = temp_dict[tag_list[4]]
+    # temp_dict.clear()
+    return connection, batch, job, tests, anomalies
 
-    con = list_1[0] + ',' + list_1[1]
-    job = list_1[2]
-    test = list_1[3] + ',' + list_1[4]
-   
 
-    li = [con,job,test]
+def json_con_batch_tests_anomalies(value_0, value_1):
+    if (len(value_0) == 0 and len(value_1) == 0):
+        sample_json_con_batch = {"type": "section"}
+    else:
+        if (value_0.startswith("tests") or value_0 == ""):
+            sample_json_con_batch = {"type": "section", "text": {"type": "mrkdwn", "text": value_0 + "\n" + value_1}}
+        else:
+            sample_json_con_batch = {"type": "section", "text": {"type": "mrkdwn", "text": value_0 + "," + value_1}}
 
-    for i in li:
-        sample_text_json = {"type": "section", "text": {"type": "mrkdwn", "text": i}}
-        temp_dict["blocks"].extend([sample_text_json, divider_json])
+    temp_json["blocks"].extend([sample_json_con_batch, divider_json])
 
-    list_1.clear()
 
-temp_json = json.dumps(temp_dict, indent=2)
-print(temp_json)
+def json_job(value_2):
+    if (len(value_2) == 0):
+        sample_json_job = {"type": "section", "text": {"type": "mrkdwn"}}
+    else:
+        sample_json_job = {"type": "section", "text": {"type": "mrkdwn", "text": value_2}}
+    temp_json["blocks"].extend([sample_json_job, divider_json])
+
+
+for input_dict in input_array:
+    values = extract_values(input_dict)
+    json_con_batch_tests_anomalies(values[0], values[1])
+    json_job(values[2])
+    json_con_batch_tests_anomalies(values[3], values[4])
+
+final_str_json = json.dumps(temp_json, indent=2)
+print(final_str_json)
